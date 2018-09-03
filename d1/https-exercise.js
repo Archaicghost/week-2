04f53ac37ad794e56563a4aa01a0a4ffa27cc329
Zoe Gelech <archaic.ghost@gmail.com>
2018-09-03 17:54:29 +0000

    Step one of exercise

---
+++ d1/https-exercise.js
@@ -0,0 +1,21 @@
+function getAndPrintHTMLChunks () {
+
+	var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+ https.get(requestOptions, function(response){
+ response.setEncoding('utf8');
+
+ response.on('data', function(data){
+ 	console.log('Chunk Received. Length:', data.length);
+ });
+
+ response.on('end', function() {
+ 	console.log('response stream complete');
+ });
+ 
+});
