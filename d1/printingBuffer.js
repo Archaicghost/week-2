a537fd6ced5eaabb21e60ad852385c158c8787b1
Zoe Gelech <archaic.ghost@gmail.com>
2018-09-03 19:05:02 +0000

    Printering Buffer Step

--- d1/printingBuffer.js
+++ d1/printingBuffer.js
@@ -1,52 +1,31 @@
-function getAndPrintHTML () {
-
 	var https = require('https');
 	var data = '';
-	var readStream = https.createReadStream('printingChunks');
+
+function getAndPrintHTML () {
 
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step2.html'
   };
 
-  readStream.setEncoding('UTF8');
-  readStream.on('data', function(chunk) {
+  https.get(requestOptions, function(response) {
+
+  response.setEncoding('UTF8');
+
+  response.on('data', function(chunk) {
   	data += chunk
   });
 
-  readStream.on('end', function() {
-  	console.log(getAndPrintHTML(data));
+  response.on('end', function() {
+  	console.log(data);
   });
 
-  readerStream.on('error', function(err) {
-  	console.log(getAndPrintHTML((err.stack);
+  response.on('error', function(err) {
+  	console.log(err.stack);
   });
 
-console.log(getAndPrintHTML(("program Ended"))
-
-	// https.request(options, callback).end()
-
+});
 
 }
 
-
-
-	var https = require('https');
-
-  var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
-  };
-
- https.get(requestOptions, function(response){
- response.setEncoding('utf8');
-
- response.on('data', function(data){
- 	console.log('Chunk Received. Length:', data.length);
- });
-
- response.on('end', function() {
- 	console.log('response stream complete');
- });
- 
-});
+getAndPrintHTML() 
